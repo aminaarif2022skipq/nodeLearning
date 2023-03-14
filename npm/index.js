fc514eb53b1154786ca8f31043e773c5b811ce5f
Amina <amina.arif.skipq@gmail.com>
2023-03-14 23:00:21 +0500

    finished learning about npm

---
+++ npm/index.js
@@ -0,0 +1,47 @@
+const {format} = require('date-fns');
+const { v4 : uuid } = require('uuid');
+
+console.log(format(new Date(),'yyyyMMdd\tHH:mm:ss'));
+
+console.log(uuid());
+
+//install a package globally
+//npm i nodemon -g
+
+//npm istall
+//install all packages defined inside package.json
+
+//to add a dependency as a dev dependency
+//npm i nodemon --save-dev
+//npm i nodemon --D
+
+//added start script and dev script
+//for scripts other than start use run
+//e.g  npm run dev
+
+//version ^2.4.5
+//here 2 means major version
+//4 means minor version
+//5 means patch
+//^ means go ahead and apply update to min and patch version
+//but don't do anything to major
+//major verison might cause breaking changes
+
+//version ~2.4.5
+// change patch but don't change minor
+
+//packagename : "*"
+//go ahead and change every thing
+
+//install a specific version
+//npm i uuid@5.6.6
+
+//npm update
+//to update minor and patch version
+
+//remove package
+//npm update
+
+
+
+
